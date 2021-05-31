e6742be13915d4d0709d26da3b8057209ea070c7
CF Mitrah <j_saravanamuthu@yahoo.com>
2021-05-31 19:27:05 +0530

    Implemented the product showing design and select products functionality with fetch call.

--- src/dataTransformer.js
+++ src/dataTransformer.js
@@ -1,19 +1,36 @@
-import get from 'lodash/get';
+import get from "lodash/get";
 
 /**
  * Transforms the API response of CommerceLayer into
  * the product schema expected by the SkuPicker component
  */
-export const dataTransformer = projectUrl => product => {
-    const { id } = product;
-    const image = get(product, ['imageUrl']) || get(product, ['attributes', 'image_url']);
-    const name = get(product, ['name']) || get(product, ['attributes', 'name']);
-    const sku = get(product, ['code']) || get(product, ['attributes', 'code']);
-    return {
-        id,
-        image,
-        name,
-        sku,
-        externalLink: `${projectUrl}/admin/skus/${id}/edit`
-    };
+export const singleProductdataTransformer = (projectUrl) => (product) => {
+  const { sku_skuID } = product;
+  const image =
+    get(product, ["imageUrl"]) ||
+    "https://lh3.googleusercontent.com/f4wX2JJcJRVbtGedb6Is06kfl1Lb4KV2p6_XAzusHS1rZ0JSH1_eUkG-0YA6u5fkSjg1FQ=s85";
+  const name = get(product, ["product_productName"]);
+  const sku = get(product, ["product_productCode"]);
+  return {
+    sku_skuID,
+    image,
+    name,
+    sku,
+    externalLink: `${projectUrl}/admin/skus/${sku_skuID}/edit`,
+  };
+};
+export const multipleProductdataTransformers = (projectUrl) => (product) => {
+  const { defaultSku_skuID } = product;
+  const image =
+    get(product, ["imageUrl"]) ||
+    "https://lh3.googleusercontent.com/f4wX2JJcJRVbtGedb6Is06kfl1Lb4KV2p6_XAzusHS1rZ0JSH1_eUkG-0YA6u5fkSjg1FQ=s85";
+  const name = get(product, ["productName"]);
+  const sku = get(product, ["productCode"]);
+  return {
+    sku_skuID: defaultSku_skuID,
+    image,
+    name,
+    sku,
+    externalLink: `${projectUrl}/admin/skus/${defaultSku_skuID}/edit`,
+  };
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,149 +1,127 @@
-import { render } from 'react-dom';
+import { render } from "react-dom";
 
-import '@contentful/forma-36-react-components/dist/styles.css';
-import '@contentful/forma-36-fcss/dist/styles.css';
-import '@contentful/forma-36-tokens/dist/css/index.css';
-import { setup, renderSkuPicker } from '@contentful/ecommerce-app-base';
-// import CLayerAuth from '@commercelayer/js-auth';
+import "@contentful/forma-36-react-components/dist/styles.css";
+import "@contentful/forma-36-fcss/dist/styles.css";
+import "@contentful/forma-36-tokens/dist/css/index.css";
+import { setup, renderSkuPicker } from "@contentful/ecommerce-app-base";
 
-import logo from './assets/slatwall.svg';
-import chunk from 'lodash/chunk';
-import flatMap from 'lodash/flatMap';
-import difference from 'lodash/difference';
-import LocalhostWarning from './components/LocalhostWarning';
-import { dataTransformer } from './dataTransformer';
+import logo from "./assets/slatwall.svg";
+import chunk from "lodash/chunk";
+import flatMap from "lodash/flatMap";
+import difference from "lodash/difference";
+import LocalhostWarning from "./components/LocalhostWarning";
+import {
+  singleProductdataTransformer,
+  multipleProductdataTransformers,
+} from "./dataTransformer";
 
-const DIALOG_ID = 'root';
+const DIALOG_ID = "root";
 const PER_PAGE = 20;
 var accessToken: any = null;
 
 function makeCTA(fieldType: any) {
-  return fieldType === 'Array' ? 'Select products' : 'Select a product';
+  return fieldType === "Array" ? "Select products" : "Select a product";
 }
 
 function validateParameters(parameters: any) {
   if (parameters.apiEndpoint.length < 1) {
-    return 'Provide the slatwall API endpoint.';
+    return "Provide the slatwall API endpoint.";
   }
+  // if (parameters.storefrontAccessToken.length < 1) {
+  //   return "Provide the storefront access token to your slatwall store.";
+  // }
   return null;
 }
 
-// async function getAccessToken() {
-//   let accessTokens: any = ''
-//   if (!accessToken) {
-//     accessTokens = (
-//       await CLayerAuth.getIntegrationToken({
-//         clientId: 'wXqRQI8me0saW5nGXpAZAGjdTfv4mf-UT7bxi7kTKE8',
-//         endpoint: 'https://the-green-brand-112.commercelayer.io',
-//         clientSecret: ''
-//       })
-//     );
-//     console.log('accessToken......', accessTokens.accessToken)
-//   }
-//   return accessTokens.accessToken;
-// }
-
-async function fetchSKUs(installationParams: any, search: any, pagination: any) {
+async function fetchSKUs(
+  installationParams: any,
+  search: any,
+  pagination: any
+) {
   const validationError = validateParameters(installationParams);
   if (validationError) {
     throw new Error(validationError);
   }
   const { apiEndpoint } = installationParams;
-  // const accessToken = await getAccessToken();
 
-  // const URL = `https://the-green-brand-112.commercelayer.io/api/skus?page[size]=${PER_PAGE}&page[number]=${pagination.offset /
-  //   PER_PAGE +
-  //   1}${search.length ? `&filter[q][name_or_code_cont]=${search}` : ''}`;
+  const URL = `${apiEndpoint}/api/scope/getProducts?currentPage=${
+    pagination.offset / PER_PAGE + 1
+  }&pageSize=${PER_PAGE}${search.length ? `&keyword=${search}` : ""}`;
 
-  // const res = await fetch(URL, {
-  //   headers: {
-  //     Accept: 'application/vnd.api+json',
-  //     Authorization: `Bearer eyJhbGciOiJIUzUxMiJ9.eyJvcmdhbml6YXRpb24iOnsiaWQiOiJ6bkJFckZKTm9YIn0sImFwcGxpY2F0aW9uIjp7ImlkIjoiYXBFbWFpV2JWcCIsImtpbmQiOiJjb250ZW50ZnVsIiwicHVibGljIjp0cnVlfSwidGVzdCI6dHJ1ZSwiZXhwIjoxNjE5MTAwNzQ5LCJyYW5kIjowLjg4ODUwNDU2MTAwMzYyOTh9.NDQKdKxY4gY-V03mikRzXUYLQVEiWAgxbx8UyYRJ8P5m4tDaFXzGG-sjRs-huOiN3aqL7Z5RSKFj9fCzOQTwkA`
-  //   },
-  //   method: 'GET'
-  // });
+  const res = await fetch(URL, {
+    headers: {
+      Accept: "application/vnd.api+json",
+    },
+    method: "GET",
+  });
 
-  // console.log('res,,,,,,,,', await res.json())
-  let response: any = []
-  let init = {
-    hostname: 'slatwall.mitrahsoft.co.in',
-    AccessKey: 'B44ACE4E36B498BFCE066FD5CF2881C5C540F570',
-    AccessKeySecret: 'ODM0MjQ4REVBNDY1ODY4REJCNTY1MkJCRDZGOTQzQUVDMDg2NEZCMw=='
-  }
-
-  const slatwall = require("../src/Slatwall/slatwall")(init)
-
-  var res = await slatwall.products.get()
-  console.log('res....', JSON.parse(res))
-  response = JSON.parse(res).pageRecords
-  console.log('response', response)
-  return response;
+  return await res.json();
 }
 
-const fetchProductPreviews = async function fetchProductPreviews(skus: any, config: any) {
-  console.log('fetchProductPreviews....')
+const fetchProductPreviews = async function fetchProductPreviews(
+  skus: any,
+  config: any
+) {
   if (!skus.length) {
     return [];
   }
 
   const PREVIEWS_PER_PAGE = 25;
-  console.log('config', config)
 
-  // const { apiEndpoint } = config;
-  // const accessToken = await getAccessToken( apiEndpoint);
+  const { apiEndpoint } = config;
 
-  const resultPromises = chunk(skus, PREVIEWS_PER_PAGE).map(async skusSubset => {
-    // const URL = `${apiEndpoint}/api/skus?page[size]=${PREVIEWS_PER_PAGE}&filter[q][code_in]=${skusSubset}`;
-    // const URL = `https://the-green-brand-112.commercelayer.io/api/skus?page[size]=${PREVIEWS_PER_PAGE}&filter[q][code_in]=${skusSubset}`;
-    const URL = `https://slatwall.mitrahsoft.co.in/api/skus?page[size]=${PREVIEWS_PER_PAGE}&filter[q][code_in]=${skusSubset}`;
-    const res = await fetch(URL, {
-      headers: {
-        Accept: 'application/vnd.api+json',
-        Authorization: `Bearer eyJhbGciOiJIUzUxMiJ9.eyJvcmdhbml6YXRpb24iOnsiaWQiOiJ6bkJFckZKTm9YIn0sImFwcGxpY2F0aW9uIjp7ImlkIjoiYXBFbWFpV2JWcCIsImtpbmQiOiJjb250ZW50ZnVsIiwicHVibGljIjp0cnVlfSwidGVzdCI6dHJ1ZSwiZXhwIjoxNjE5MTAwNzQ5LCJyYW5kIjowLjg4ODUwNDU2MTAwMzYyOTh9.NDQKdKxY4gY-V03mikRzXUYLQVEiWAgxbx8UyYRJ8P5m4tDaFXzGG-sjRs-huOiN3aqL7Z5RSKFj9fCzOQTwkA`
-      },
-      method: 'GET'
-    });
-    return await res.json();
-  });
+  const resultPromises = chunk(skus, PREVIEWS_PER_PAGE).map(
+    async (skusSubset) => {
+      const URL = `${apiEndpoint}/api/public/Product?f:publishedFlag=true&f:productCode:in=${skusSubset}&pageSize=${PREVIEWS_PER_PAGE}`;
+      const res = await fetch(URL, {
+        headers: {
+          Accept: "application/vnd.api+json",
+        },
+        method: "GET",
+      });
+      return await res.json();
+    }
+  );
 
   const results = await Promise.all(resultPromises);
 
-  const foundProducts = flatMap(results, ({ data }) =>
-    // data.map(dataTransformer(config.apiEndpoint))
-    data.map(dataTransformer('https://the-green-brand-112.commercelayer.io'))
-  );
+  const foundProducts = flatMap(results, ({ data }) => {
+    return data.map(multipleProductdataTransformers(apiEndpoint));
+  });
 
   const missingProducts = difference(
     skus,
-    foundProducts.map(product => product.sku)
-  ).map(sku => ({ sku, isMissing: true, image: '', name: '', id: '' }));
-
+    foundProducts.map((product) => product.sku)
+  ).map((sku) => ({ sku, isMissing: true, image: "", name: "", id: "" }));
   return [...foundProducts, ...missingProducts];
 };
 
 async function renderDialog(sdk: any) {
-  const container = document.createElement('div');
-  container.style.display = 'flex';
-  container.style.flexDirection = 'column';
+  const container = document.createElement("div");
+  container.style.display = "flex";
+  container.style.flexDirection = "column";
 
   renderSkuPicker(DIALOG_ID, {
     sdk,
     fetchProductPreviews,
     fetchProducts: async (search, pagination) => {
-      const result = await fetchSKUs(sdk.parameters.installation, search, pagination);
+      const result = await fetchSKUs(
+        sdk.parameters.installation,
+        search,
+        pagination
+      );
       return {
         pagination: {
           count: PER_PAGE,
           limit: PER_PAGE,
-          // total: result.meta.record_count,
-          total: 10,
-          offset: 1
+          total: result.data.total,
+          offset: 1,
         },
-        // products: result.data.map(dataTransformer(sdk.parameters.installation.apiEndpoint))
-        // products: result.data.map(dataTransformer('https://the-green-brand-112.commercelayer.io'))
-        products: []
+        products: result.data.products.map(
+          singleProductdataTransformer(sdk.parameters.installation.apiEndpoint)
+        ),
       };
-    }
+    },
   });
 
   sdk.window.startAutoResizer();
@@ -152,12 +130,12 @@ async function renderDialog(sdk: any) {
 async function openDialog(sdk: any, currentValue: any, config: any) {
   const skus = await sdk.dialogs.openCurrentApp({
     allowHeightOverflow: true,
-    position: 'center',
+    position: "center",
     title: makeCTA(sdk.field.type),
     shouldCloseOnOverlayClick: true,
     shouldCloseOnEscapePress: true,
     parameters: config,
-    width: 1400
+    width: 1400,
   });
 
   return Array.isArray(skus) ? skus : [];
@@ -168,32 +146,33 @@ function isDisabled(/* currentValue, config */) {
   return false;
 }
 
-if (process.env.NODE_ENV === 'development' && window.self === window.top) {
+if (process.env.NODE_ENV === "development" && window.self === window.top) {
   // You can remove this if block before deploying your app
-  const root = document.getElementById('root');
+  const root = document.getElementById("root");
   render(<LocalhostWarning />, root);
 } else {
   setup({
     makeCTA,
-    name: 'Slatwall',
+    name: "Slatwall",
     logo,
-    color: '#212F3F',
-    description: 'The Slatwall app allows editors to select products from their Slatwall account and reference them inside of Contentful entries.',
+    color: "#212F3F",
+    description:
+      "The Slatwall app allows editors to select products from their Slatwall account and reference them inside of Contentful entries.",
     parameterDefinitions: [
-      // {
-      //   id: 'storefrontAccessToken',
-      //   name: 'Storefront Access Token',
-      //   description: 'The storefront access token to your Slatwall store',
-      //   type: 'Symbol',
-      //   required: true
-      // },
       {
-        id: 'apiEndpoint',
-        name: 'API Endpoint',
-        description: 'The Slatwall API endpoint',
-        type: 'Symbol',
-        required: true
-      }
+        id: "storefrontAccessToken",
+        name: "Storefront Access Token",
+        description: "The storefront access token to your Slatwall store",
+        type: "Symbol",
+        required: false,
+      },
+      {
+        id: "apiEndpoint",
+        name: "API Endpoint",
+        description: "The Slatwall API endpoint",
+        type: "Symbol",
+        required: true,
+      },
     ],
     validateParameters: validateParameters,
     fetchProductPreviews,
